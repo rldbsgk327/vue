d1813523ea8eafa611a049919454146bb906e431
rldbsgk327 <rldbsgk327@gmail.com>
2020-05-06 10:20:47 +0900

    4번째

--- skhu-kyh/public/mid_test/mid5.js
+++ skhu-kyh/public/mid_test/mid5.js
@@ -1,11 +1,14 @@
 let a = [ "hello.html", "world.js", "readme.txt" ];
 
 function getExtension(fileName){
-    fileName = 
     let b = fileName.indexof('.');
     let result = fileName.slice(b);
     return result;
 }
 
-console.log(getExtension(a[0]));
 
+
+for(let i=0; i<3; i++){
+    fileName = a[i];
+    console.log(getExtension(fileName));
+}
