b19484ba6075514adfcf0d5ba9cfb6b33894f11a
rldbsgk327 <rldbsgk327@gmail.com>
2020-06-30 16:21:49 +0900

    버튼비활성화

--- vue_app2/src/pages/page3/App.vue
+++ vue_app2/src/pages/page3/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
     <h1>페이지3</h1>
+    <Clock v-if="showTimer"></Clock>
+    <div>
+      <button type="button" @click="showTimer = true" 
+                           :disabled="showTimer">시계 보이기</button>
+      <button type="button" @click="showTimer = false"
+                            :disabled="!showTimer">시계 감추기</button>
+    </div>
   </div>
 </template>
 
 <script>
+import Clock from './Clock.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data: function() {
+    return {
+      showTimer: true
+    }
+  },
+  components: { Clock }
 }
 </script>
 
+<style scoped>
+div#app { padding: 0 30px 30px 30px; margin: 30px auto; max-width: 400px;
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; }
+</style>
---
+++ vue_app2/src/pages/page3/Clock.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ time }}</div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "Clock",
+  data: function() {
+    return { time: null };
+  },
+  mounted: function() {
+    let callback = () => this.time = moment().format("HH:mm:ss")
+    callback();
+    this.timerId = setInterval(callback, 1000);
+    
+  },
+  beforeDestroy: function(){
+      clearInterval(this.timerId);
+  }
+}
+</script>
+
+<style scoped>
+div { display: inline-block; font-size: 15pt; padding:3px 12px; border: 1px solid gray; }
+</style>
